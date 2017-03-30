97bd98f0fcb45686c196b3bac81d8a7ea48c651d
Thanh TRAN <ttran@advanced-schema.com>
2017-03-30 22:55:29 +0200

    Update usage of BST

--- BST/main.c
+++ BST/main.c
@@ -7,15 +7,13 @@ int main() {
     struct bst *tree = NULL;
     struct bst *found_node = malloc(sizeof(*found_node));
 
-    int nbr[10] = {54, 65 ,87 ,23, 53, 32, 13, 53, 98, 34};
+    int nbr[12] = {54, 65 ,87 ,23, 77, 32, 13, 53, 98, 34, 84, 91};
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 12; ++i) {
         push(&tree, nbr[i]);
     }
 
-    found_node = search(tree, 53);
+    found_node = search(tree, 91);
     printf("%d\n", found_node->number);
-    printf("%d\n", found_node->left->number);
-    printf("%d\n", found_node->right->number);
     return 0;
 }
