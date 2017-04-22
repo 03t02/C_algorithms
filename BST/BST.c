78ed7c87a6c68810d7c15203287d8e3364ebed79
Thanh TRAN <ttran@advanced-schema.com>
2017-04-22 13:36:53 +0200

    Minimal optimization

--- BST/BST.c
+++ BST/BST.c
@@ -15,14 +15,14 @@ void invertTree(t_bst **tree) {
 }
 
 void push(t_bst **tree, int number) {
-    t_bst *new_node = NULL;
-
-    new_node = malloc(sizeof(*new_node));
-    new_node->number = number;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
     if (*tree == NULL) {
+        t_bst *new_node = NULL;
+
+        new_node = malloc(sizeof(*new_node));
+        new_node->number = number;
+        new_node->left = NULL;
+        new_node->right = NULL;
+
         *tree = new_node;
     } else {
         if (number < (*tree)->number) {
